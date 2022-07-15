5ae2e813bca0cc37d08ed6532714c6848147a7c0
Lexyna <lexieverena@outlook.de>
2022-07-15 14:27:25 +0200

    added more splitter tests

--- tests/core/predefined/splitterConnections.ts
+++ tests/core/predefined/splitterConnections.ts
@@ -60,3 +60,128 @@ export const addingTwoNumberConnectionAfterSplit1: EngineConnections = {
         }
     }
 }
+
+export const addingThreeNumberConnectionAfterSplit2: EngineConnections = {
+    input: {
+        "rootINPUT0": {
+            self: {
+                ioId: "rootINPUT0",
+                nodeId: "root",
+                type: CONNECTION_TYPE.INPUT,
+                index: 0,
+            },
+            connections: [
+                {
+                    ioId: "addEngineNode2OUTPUT0",
+                    nodeId: "addEngineNode2",
+                    type: CONNECTION_TYPE.OUTPUT,
+                    index: 0
+                }
+            ]
+        },
+        "addEngineNode1INPUT0": {
+            self: {
+                ioId: "addEngineNode1INPUT0",
+                nodeId: "addEngineNode1",
+                type: CONNECTION_TYPE.INPUT,
+                index: 0,
+            },
+            connections: [
+                {
+                    ioId: "constOneEngineNodeOUTPUT0",
+                    nodeId: "constOneEngineNode",
+                    type: CONNECTION_TYPE.OUTPUT,
+                    index: 0
+                }
+            ]
+        },
+        "addEngineNode1INPUT1": {
+            self: {
+                ioId: "addEngineNode1INPUT1",
+                nodeId: "addEngineNode1",
+                type: CONNECTION_TYPE.INPUT,
+                index: 1,
+            },
+            connections: [
+                {
+                    ioId: "constOneEngineNodeOUTPUT0",
+                    nodeId: "constOneEngineNode",
+                    type: CONNECTION_TYPE.OUTPUT,
+                    index: 0
+                }
+            ]
+        },
+        "addEngineNode2INPUT0": {
+            self: {
+                ioId: "addEngineNode2INPUT0",
+                nodeId: "addEngineNode2",
+                type: CONNECTION_TYPE.INPUT,
+                index: 0,
+            },
+            connections: [
+                {
+                    ioId: "addEngineNode1OUTPUT0",
+                    nodeId: "addEngineNode1",
+                    type: CONNECTION_TYPE.INPUT,
+                    index: 0
+                }
+            ]
+        },
+    },
+    output: {
+        "constOneEngineNodeOUTPUT0": {
+            self: {
+                ioId: "constOneEngineNodeOUTPUT0",
+                nodeId: "constOneEngineNode",
+                type: CONNECTION_TYPE.OUTPUT,
+                index: 0
+            },
+            connections: [
+                {
+                    ioId: "addEngineNode1INPUT1",
+                    nodeId: "addEngineNode1",
+                    type: CONNECTION_TYPE.INPUT,
+                    index: 1,
+                },
+                {
+                    ioId: "addEngineNode1INPUT0",
+                    nodeId: "addEngineNode1",
+                    type: CONNECTION_TYPE.INPUT,
+                    index: 0,
+                }
+            ]
+        },
+        "addEngineNode1OUTPUT0": {
+            self: {
+                ioId: "addEngineNode1OUTPUT0",
+                nodeId: "addEngineNode1",
+                type: CONNECTION_TYPE.OUTPUT,
+                index: 0
+            },
+            connections: [
+                {
+                    ioId: "addEngineNode2INPUT0",
+                    nodeId: "addEngineNode2",
+                    type: CONNECTION_TYPE.INPUT,
+                    index: 0,
+                }
+            ]
+        },
+        "addEngineNode2OUTPUT0": {
+            self: {
+                ioId: "addEngineNode2OUTPUT0",
+                nodeId: "addEngineNode2",
+                type: CONNECTION_TYPE.OUTPUT,
+                index: 0
+            },
+            connections: [
+                {
+                    ioId: "rootINPUT0",
+                    nodeId: "root",
+                    type: CONNECTION_TYPE.INPUT,
+                    index: 0,
+                }
+            ]
+        }
+    }
+}
--- tests/core/splitter.test.ts
+++ tests/core/splitter.test.ts
@@ -1,17 +1,78 @@
+import { connector } from "../../src/core/engine/Connector"
 import { splitter } from "../../src/core/engine/Splitter"
-import { addEngineNode1OUTPUT0, rootINPUT0 } from "./predefined/ConnectionDetails"
-import { addingTwoNumberConnectionAfterSplit1 } from "./predefined/splitterConnections"
-import { addingTwoNumberConnection } from "./predefined/ValidConnections"
+import { addEngineNode1OUTPUT0, addEngineNode2INPUT1, constFiveEngineNodeOUTPUT0, constOneEngineNodeOUTPUT0, constThreeEngineNodeOUTPUT0, constTwoEngineNodeOUTPUT0, mulEngineNode1INPUT0, mulEngineNode1INPUT1, mulEngineNode1OUTPUT0, mulEngineNode2INPUT0, mulEngineNode2INPUT1, mulEngineNode2OUTPUT0, rootINPUT0 } from "./predefined/ConnectionDetails"
+import { addingThreeNumberConnectionAfterSplit2, addingTwoNumberConnectionAfterSplit1 } from "./predefined/splitterConnections"
+import { addingThreeNumberConnection, addingTwoNumberConnection } from "./predefined/ValidConnections"
 
 describe("Splitter tests", () => {
 
-    test("remove simple connection split1", () => {
+    test("remove simple connection (split1)", () => {
 
         const res = Object.assign({}, addingTwoNumberConnection);
 
-        splitter(addEngineNode1OUTPUT0, rootINPUT0, res);
+        const success = splitter(addEngineNode1OUTPUT0, rootINPUT0, res);
 
         expect(res).toEqual(addingTwoNumberConnectionAfterSplit1);
+        expect(success).toBe(true);
+
+    })
+
+    test("remove addingThreeNumberConnection (split2)", () => {
+
+        const res = Object.assign({}, addingThreeNumberConnection);
+
+        const success = splitter(constOneEngineNodeOUTPUT0, addEngineNode2INPUT1, res);
+
+        expect(res).toEqual(addingThreeNumberConnectionAfterSplit2);
+        expect(success).toBe(true);
+    })
+
+    test("create multiplying three number connection, then split", () => {
+
+        //connection dicts
+        const connectionDict = {
+            input: {},
+            output: {}
+        }
+
+        //connect  result form mul Node 2 to root
+        connector(mulEngineNode2OUTPUT0, rootINPUT0, connectionDict);
+
+        //connect result from mul node 1 with mul node 2
+        connector(mulEngineNode1OUTPUT0, mulEngineNode2INPUT1, connectionDict);
+
+        //connect const 2 with mul node 2
+        connector(constTwoEngineNodeOUTPUT0, mulEngineNode2INPUT0, connectionDict);
+
+        //connect const 3 with mul node 1
+        connector(constThreeEngineNodeOUTPUT0, mulEngineNode1INPUT0, connectionDict);
+
+        //connect const 5 with mul node 1
+        connector(constFiveEngineNodeOUTPUT0, mulEngineNode1INPUT1, connectionDict);
+
+        //result
+
+        const res = {
+            input: {},
+            output: {}
+        }
+
+        //connect  result form mul Node 2 to root
+        connector(mulEngineNode2OUTPUT0, rootINPUT0, res);
+
+        //connect result from mul node 1 with mul node 2
+        connector(mulEngineNode1OUTPUT0, mulEngineNode2INPUT1, res);
+
+        //connect const 2 with mul node 2
+        connector(constTwoEngineNodeOUTPUT0, mulEngineNode2INPUT0, res);
+
+        //connect const 3 with mul node 1
+        connector(constThreeEngineNodeOUTPUT0, mulEngineNode1INPUT0, res);
+
+        const success = splitter(constFiveEngineNodeOUTPUT0, mulEngineNode1INPUT1, connectionDict);
+
+        expect(connectionDict).toEqual(res);
+        expect(success).toBe(true);
 
     })
 
