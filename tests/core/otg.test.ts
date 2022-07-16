2f20add971e562e069f102f3618f6dc23c1b84e3
Lexyna <lexieverena@outlook.de>
2022-07-16 21:17:05 +0200

    Added complex arithmetic connection test

--- tests/core/otg.test.ts
+++ tests/core/otg.test.ts
@@ -2,7 +2,7 @@ import { connector } from "../../src/core/connections/Connector";
 import { EngineConnections } from "../../src/core/connections/EngineConnections";
 import { executeGraph } from "../../src/core/engine/OTG";
 import { configDict, resetTestValue, testValue } from "./predefined/ConfigNodes";
-import { constFiveEngineNodeOUTPUT0, constThreeEngineNodeOUTPUT0, constTwoEngineNodeOUTPUT0, mulEngineNode1INPUT0, mulEngineNode1INPUT1, mulEngineNode1OUTPUT0, mulEngineNode2INPUT0, mulEngineNode2INPUT1, mulEngineNode2OUTPUT0, rootINPUT0 } from "./predefined/ConnectionDetails";
+import { addEngineNode1INPUT0, addEngineNode1INPUT1, addEngineNode1OUTPUT0, addEngineNode2INPUT0, addEngineNode2INPUT1, addEngineNode2OUTPUT0, constFiveEngineNodeOUTPUT0, constFourEngineNodeOUTPUT0, constOneEngineNodeOUTPUT0, constThreeEngineNodeOUTPUT0, constTwoEngineNodeOUTPUT0, mulEngineNode1INPUT0, mulEngineNode1INPUT1, mulEngineNode1OUTPUT0, mulEngineNode2INPUT0, mulEngineNode2INPUT1, mulEngineNode2OUTPUT0, rootINPUT0, subEngineNode2INPUT0, subEngineNode2INPUT1, subEngineNode2OUTPUT0 } from "./predefined/ConnectionDetails";
 import { engineNodeDict, initializeNodeValues } from "./predefined/EngineNodes";
 import { addingThreeNumberConnection, addingTwoNumberConnection, simpleValidConnection } from "./predefined/ValidConnections";
 
@@ -54,6 +54,52 @@ describe("otg test", () => {
 
         executeGraph(configDict, engineNodeDict, connectionDict, "root");
         expect(testValue).toBe(30)
+
+    })
+
+    test("complex arithmetic calculation", () => {
+
+        const connectionDict: EngineConnections = {
+            input: {},
+            output: {}
+        }
+
+        //connect res from mul Node 2 to root
+        connector(mulEngineNode2OUTPUT0, rootINPUT0, connectionDict);
+
+        //connect sub Engine 2 output to mulEngine input
+        connector(subEngineNode2OUTPUT0, mulEngineNode2INPUT0, connectionDict);
+
+        //connect add Engine 1 output to mulEngine Input 1
+        connector(addEngineNode1OUTPUT0, mulEngineNode2INPUT1, connectionDict);
+
+        //connect add engine 2 output to add Engine 1 input 1
+        connector(addEngineNode2OUTPUT0, addEngineNode1INPUT0, connectionDict);
+
+        //connect mul engine 1 output to add engine 1
+        connector(mulEngineNode1OUTPUT0, addEngineNode1INPUT1, connectionDict);
+
+        //connect const one output to sub engine 2
+        connector(constOneEngineNodeOUTPUT0, subEngineNode2INPUT0, connectionDict);
+
+        //connect const five to sub engine 2
+        connector(constFiveEngineNodeOUTPUT0, subEngineNode2INPUT1, connectionDict);
+
+        //connect const five to add engine 2
+        connector(constFiveEngineNodeOUTPUT0, addEngineNode2INPUT0, connectionDict);
+
+        //connect const three to add engine 2
+        connector(constThreeEngineNodeOUTPUT0, addEngineNode2INPUT1, connectionDict);
+
+        //connect const four to mul engine 1
+        connector(constFourEngineNodeOUTPUT0, mulEngineNode1INPUT0, connectionDict);
+
+        //connect const two to mul engine 1
+        connector(constTwoEngineNodeOUTPUT0, mulEngineNode1INPUT1, connectionDict);
+
+        // ((4 * 2) + ( 5 + 3 )) * (1 - 5) = -64
+        executeGraph(configDict, engineNodeDict, connectionDict, "root");
+        expect(testValue).toBe(-64)
 
     })
 
