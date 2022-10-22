08442394899d5f06246d0758df6b2959f10a7e12
Lexyna <lexieverena@outlook.de>
2022-10-22 19:35:22 +0200

    Created default graph for ThreeJs nodeEditor

--- doc/graph.exe/src/NodeConfigs/ThreeJsgraph/EditorConfig.ts
+++ doc/graph.exe/src/NodeConfigs/ThreeJsgraph/EditorConfig.ts
@@ -1,5 +1,5 @@
-import { EngineConnections } from "graph.exe-core";
-import { buildEngineNode, ProtoEngineNodeDict, ProtoNodeDict } from "graph.exe-react";
+import { connector, EngineConnections, extractor } from "graph.exe-core";
+import { buildEngineNode, ProtoEngineNode, ProtoEngineNodeDict, ProtoNodeDict } from "graph.exe-react";
 import { colorNode, geometryNode, meshNode, rootNode } from "./ConfigNodes";
 
 
@@ -12,15 +12,43 @@ export const threeConfigDict: ProtoNodeDict = {
 
 const threeRootEngineNode = buildEngineNode(rootNode, true);
 
-threeRootEngineNode.position.y = 70;
+threeRootEngineNode.position.y = 200;
+threeRootEngineNode.position.x = 800;
 
 export const threeRootEngineNodeId: string = threeRootEngineNode.id;
 
+const meshEngineNode: ProtoEngineNode = buildEngineNode(meshNode);
+
+meshEngineNode.position.y = 150;
+meshEngineNode.position.x = 600;
+
+const colorEngineNode: ProtoEngineNode = buildEngineNode(colorNode);
+
+colorEngineNode.position.y = 120;
+colorEngineNode.position.x = 400;
+
+const geometryEngineNode: ProtoEngineNode = buildEngineNode(geometryNode);
+
+geometryEngineNode.position.x = 400;
+geometryEngineNode.position.y = 280;
+
 export const threeNodes: ProtoEngineNodeDict = {
     [threeRootEngineNode.id]: threeRootEngineNode,
+    [meshEngineNode.id]: meshEngineNode,
+    [colorEngineNode.id]: colorEngineNode,
+    [geometryEngineNode.id]: geometryEngineNode
 }
 
 export const threeConnections: EngineConnections = {
     input: {},
     output: {}
 }
+
+const previewPorts = extractor(threeRootEngineNode);
+const meshPorts = extractor(meshEngineNode);
+const colorPorts = extractor(colorEngineNode);
+const geometryPorts = extractor(geometryEngineNode);
+
+connector(meshPorts.outputs[0], previewPorts.inputs[0], threeConnections);
+connector(colorPorts.outputs[0], meshPorts.inputs[0], threeConnections);
+connector(geometryPorts.outputs[0], meshPorts.inputs[1], threeConnections);
